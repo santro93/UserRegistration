92930ca9fd04f6bfd9889cd7dc4ccc6dea4e7457
santro93 <nsantosh435@gmail.com>
2022-04-07 23:26:59 +0530

    Welcome msg

---
+++ regex/UserRegistration.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	Scanner scanner = new Scanner(System.in);
+	public void validFirstName() {
+			System.out.println("Enter First name starts with Cap and has minimum 3 characters: ");
+			String firstName = scanner.next();
+			System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", firstName));
+		}
+	public static void main(String[] args) {
+		System.out.println("Welcometo User Registration Problem!");
+		UserRegistration userRegistration = new UserRegistration();
+		userRegistration.validFirstName();
+		
+	}
+}
