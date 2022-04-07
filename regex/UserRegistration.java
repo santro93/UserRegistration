0ed4eb54633685db8a143330bf65f161ef5b035d
santro93 <nsantosh435@gmail.com>
2022-04-07 23:45:15 +0530

    User need to enter a valid First Name

--- regex/UserRegistration.java
+++ regex/UserRegistration.java
@@ -5,15 +5,24 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	Scanner scanner = new Scanner(System.in);
+
 	public void validFirstName() {
-			System.out.println("Enter First name starts with Cap and has minimum 3 characters: ");
-			String firstName = scanner.next();
-			System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", firstName));
-		}
+		System.out.println("Enter First name starts with Cap and has minimum 3 characters: ");
+		String firstName = scanner.next();
+		System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", firstName));
+	}
+	
+	public void validLastName() {
+		System.out.println("Enter Last name starts with Cap and has minimum 3 characters: ");
+		String lastName = scanner.next();
+		System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", lastName));
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcometo User Registration Problem!");
 		UserRegistration userRegistration = new UserRegistration();
 		userRegistration.validFirstName();
-		
+		userRegistration.validLastName();
+
 	}
 }
