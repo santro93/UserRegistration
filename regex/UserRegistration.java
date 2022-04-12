85fbfff2cf98c733d0e7a2cf51f436b770c6ff18
santro93 <nsantosh435@gmail.com>
2022-04-12 23:36:02 +0530

    Should clear all email samples provided separately

--- regex/UserRegistration.java
+++ regex/UserRegistration.java
@@ -18,12 +18,6 @@ public class UserRegistration {
 		System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", lastName));
 	}
 
-	public void validEmail() {
-		System.out.print("Enter email Id : ");
-		String email = scanner.next();
-		System.out.println(Pattern.matches("^(abc)[.]{1}[A-Za-z]*[@]{1}(bl.co)(.in)*$", email));
-	}
-
 	public void validPhoneNumber() {
 		System.out.print("Enter phone number : ");
 		String phone = scanner.next();
@@ -33,15 +27,29 @@ public class UserRegistration {
 	public void validPassword() {
 		System.out.print("Enter password : ");
 		String password = scanner.next();
-		 System.out.println(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}$", password));	}
+		System.out.println(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}$", password));
+	}
+
+	public void validEmail(String[] email) {
+		for (int i = 0; i < email.length; i++) {
+			System.out.print(Pattern.matches("^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", email[i]) + " ");
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Problem!");
+
+		String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+
 		UserRegistration userRegistration = new UserRegistration();
 		userRegistration.validFirstName();
 		userRegistration.validLastName();
-		userRegistration.validEmail();
 		userRegistration.validPhoneNumber();
 		userRegistration.validPassword();
+		System.out.println("Valid email :");
+		userRegistration.validEmail(validEmailSample);
+		System.out.println("\nInvalid emails :");
+		userRegistration.validEmail(invalidEmailSample);
 	}
 }
