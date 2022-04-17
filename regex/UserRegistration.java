45b03c80968ee9698cf07bc5e686b9c2a635dea3
santro93 <nsantosh435@gmail.com>
2022-04-17 15:13:26 +0530

    JUnit Test to validate the User Entry

--- regex/UserRegistration.java
+++ regex/UserRegistration.java
@@ -1,55 +1,30 @@
 package com.bridgelabz.regex;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	Scanner scanner = new Scanner(System.in);
+	private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
+	private static final String EMAIL = "^(abc)[.][a-z]+[@](bl)[.](co)[.][a-z]+$";
+	private static final String PHONE_NUMBER = "^(91)[\\s]{1}[1-9]{1}[0-9]{9}$";
+	private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=[^!@#$%^&+=]*[!@#$%^&+=][^!@#$%^&+=]*$)(?=.*[0-9]).{8,}$";
 
-	public void validFirstName() {
-		System.out.println("Enter First name starts with Cap and has minimum 3 characters: ");
-		String firstName = scanner.next();
-		System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", firstName));
+	public boolean validFirstName(String firstName) {
+		return Pattern.matches(NAME, firstName);
 	}
 
-	public void validLastName() {
-		System.out.println("Enter Last name starts with Cap and has minimum 3 characters: ");
-		String lastName = scanner.next();
-		System.out.println(Pattern.matches("^[A-Z]{1}[A-Za-z]{2,}$", lastName));
+	public boolean validLastName(String lastName) {
+		return Pattern.matches(NAME, lastName);
 	}
 
-	public void validPhoneNumber() {
-		System.out.print("Enter phone number : ");
-		String phone = scanner.next();
-		System.out.println(Pattern.matches("(0|91)?[7-9][0-9]{9}", phone));
+	public boolean validEmail(String email) {
+		return Pattern.matches(EMAIL, email);
 	}
 
-	public void validPassword() {
-		System.out.print("Enter password : ");
-		String password = scanner.next();
-		System.out.println(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}$", password));
+	public boolean validPhoneNumber(String phoneNumber) {
+		return Pattern.matches(PHONE_NUMBER, phoneNumber);
 	}
 
-	public void validEmail(String[] email) {
-		for (int i = 0; i < email.length; i++) {
-			System.out.print(Pattern.matches("^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", email[i]) + " ");
-		}
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Problem!");
-
-		String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
-        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
-
-		UserRegistration userRegistration = new UserRegistration();
-		userRegistration.validFirstName();
-		userRegistration.validLastName();
-		userRegistration.validPhoneNumber();
-		userRegistration.validPassword();
-		System.out.println("Valid email :");
-		userRegistration.validEmail(validEmailSample);
-		System.out.println("\nInvalid emails :");
-		userRegistration.validEmail(invalidEmailSample);
+	public boolean validPassword(String password) {
+		return Pattern.matches(PASSWORD, password);
 	}
 }
---
+++ regexjunit/UserRegistrationTest.java
@@ -0,0 +1,70 @@
+package com.bridgelabz.regexjunit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.regex.UserRegistration;
+
+public class UserRegistrationTest {
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.validFirstName("Santosh");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenFirstName_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.validFirstName("san");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void givenLastName_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.validLastName("Nagargoje");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenLastName_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.validLastName("nagaRgoje");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void givenEmail_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.validEmail("abc.xyz@bl.co.in");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenEmail_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.validEmail("abc.xyz@bl.in");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void givenPhone_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.validPhoneNumber("91 9922874588");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenPhone_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.validPhoneNumber("919832373");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void givenPassword_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.validPassword("Pqr@12345");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenPassword_whenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.validPassword("asdfg@#$%");
+		Assert.assertFalse(result);
+	}
+}
